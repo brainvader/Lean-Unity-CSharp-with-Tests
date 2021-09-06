58eda3ca7124a973e38380399212e83880c3f94f
brainvader <brainvade@gmail.com>
2021-09-07 00:04:27 +0900

    Add new test case

--- Assets/Tests/HelloTest.cs
+++ Assets/Tests/HelloTest.cs
@@ -22,4 +22,12 @@ public class HelloTest
         // Use the Assert class to test conditions
         Assert.AreEqual(want, got);
     }
+
+    [Test]
+    public void SayHelloWorldWhenEmptyString() {
+        var got = hello.message;
+        var want = "Hello, World";
+
+        Assert.AreEqual(got, want);
+    }
 }

b018baa76b8c31fd2a85fc8593ef3e265e9b53d8
brainvader <brainvade@gmail.com>
2021-09-06 23:40:54 +0900

    Add constant value

--- Assets/Scripts/Hello.cs
+++ Assets/Scripts/Hello.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Hello 
 {
     public string message;
+    private const string englishHelloPrefix = "Hello, ";
 
     public Hello(string message) {
         this.message = message;

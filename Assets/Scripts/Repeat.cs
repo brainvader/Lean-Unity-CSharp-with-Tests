109d234253df9d9687a14da60da9dfc4cab2eaf3
brainvader <brainvade@gmail.com>
2021-09-08 18:30:54 +0900

    Update test target to pass test

--- Assets/Scripts/Repeat.cs
+++ Assets/Scripts/Repeat.cs
@@ -3,4 +3,11 @@ public class Repeat {
     public Repeat(char letter) {
         this.letter = letter;
     }
+    public string letters() {
+        var repeated = "";
+        for (var i = 0; i < 5; i++) {
+            repeated = repeated + this.letter;
+        }
+        return repeated;
+    }
 }

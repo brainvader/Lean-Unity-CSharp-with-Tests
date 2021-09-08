0bfa3c2e3f6ecb6ed3c1c43ce5f9a43f9e675db7
brainvader <brainvade@gmail.com>
2021-09-08 18:30:28 +0900

    Update test code

--- Assets/Tests/IterationTest.cs
+++ Assets/Tests/IterationTest.cs
@@ -3,7 +3,8 @@ using NUnit.Framework;
 public class IterationTest {
     [Test]
     public void RepeatLetterFiveTimes() {
-        var repeated = new Repeat('a');
+        var repeat = new Repeat('a');
+        var repeated = repeat.letters();
         var expected = "aaaaa";
 
         Assert.AreEqual(repeated, expected);
